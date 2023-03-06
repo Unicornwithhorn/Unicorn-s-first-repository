3665dc0e6cd6a5166f0f7de2c2fcc21df15c7cc2
Sergey Chernorutsky <sergeychernorutsky@gmail.com>
2023-03-06 22:59:30 +0300

    Add Example 5, name choice with if\else

---
+++ Example005_HiMyFavorite/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "вася")
+{
+  Console.WriteLine("Привет Васюся");
+}
+else
+{
+  Console.Write("Привет");
+  Console.Write(username);
+}
