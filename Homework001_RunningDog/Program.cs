1fe0bd80bfb6ba3646c86342c9d6c1ee17a7632d
Sergey Chernorutsky <sergeychernorutsky@gmail.com>
2023-03-07 12:29:30 +0300

    Homework_RunningDog was added

---
+++ Homework001_RunningDog/Program.cs
@@ -0,0 +1,20 @@
+﻿int count = 0, distance = 10000, FirstFriendSpeed = 1, SecondFriendSpeed = 2, DogSpeed = 5, friends = 2, time = 0;
+while (distance > 10)
+{
+  if (friends == 1)
+  {
+    time=distance/(FirstFriendSpeed+DogSpeed);
+    friends=2;
+  }
+  if (friends == 2)
+  {
+    time=distance/(SecondFriendSpeed+DogSpeed);
+    friends=1;
+  }
+  distance=distance-(FirstFriendSpeed+SecondFriendSpeed)*time;
+  count++;
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз");
